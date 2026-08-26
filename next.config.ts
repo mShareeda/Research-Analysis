7372ca33f68aee66b01f7ca8ad8b047fee774887
Mohamed Shareeda <m.shareeda@gmail.com>
2026-08-26 15:48:55 +0300

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  serverExternalPackages: ["pdf-parse", "mammoth"],
-  allowedDevOrigins: ["10.2.3.44"],
-};
-
-export default nextConfig;
